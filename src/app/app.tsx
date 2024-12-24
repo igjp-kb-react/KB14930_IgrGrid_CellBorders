ac03fc49d8d04037f2ad9e9085c8808a64fa212e
norikois <33947244+norikois@users.noreply.github.com>
2024-12-24 11:21:10 +0900

    Initial commit
    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,21 @@
+import { IgrColumn, IgrGrid } from "@infragistics/igniteui-react-grids";
+import "./app.css";
+
+const sampleData = [
+  { id: 1, name: "John", country: "USA", age: 37 },
+  { id: 2, name: "Alice", country: "India", age: 32 },
+  { id: 3, name: "Bob", country: "Korea", age: 25 },
+  { id: 4, name: "Doe", country: "Japan", age: 20 },
+];
+
+export const App = () => {
+  return (
+    <div style={{ maxWidth: "600px", margin: "24px auto" }}>
+      <IgrGrid data={sampleData} primaryKey="id" width="100%" height="300px" style={{ "--ig-size": "var(--ig-size-small)" }}>
+        <IgrColumn field="name" header="Name" />
+        <IgrColumn field="country" header="Country" />
+        <IgrColumn field="age" header="Age" />
+      </IgrGrid>
+    </div>
+  );
+};
